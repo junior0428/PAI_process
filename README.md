c2cc918f38cdfc1c6234016b50d71ffe8dbaa5f8
junior0428 <junior.calvo04@gmail.com>
2025-03-19 01:13:50 +0100

    class ectraccion

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# PAI_process
-PAI_process
+## Environment Setup
+### To create and activate this Conda environment, run:
+```
+conda env create -f conda_env.yml
+conda activate feature-matching
+conda env update -f conda_env.yml --prune
+
+```
--- utils/D2_extraccion.py
+++ utils/D2_extraccion.py
@@ -178,38 +178,11 @@ class TabD2(QWidget):
             if self.image is None:
                 QMessageBox.critical(self, "Error", "No se pudo cargar la imagen.")
                 return
+            
             self.show_image(self.image, self.label_image)
             self.kp, self.des = None, None
         else:
             QMessageBox.warning(self, "Atención", "No se seleccionó ninguna imagen.")
-
-    def load_imageA(self):
-        file_path, _ = QFileDialog.getOpenFileName(
-            self, "Abrir Imagen A", "", "Imágenes (*.png *.jpg *.jpeg *.bmp)"
-        )
-        if file_path:
-            self.imageA = cv2.imread(file_path)
-            if self.imageA is None:
-                QMessageBox.critical(self, "Error", "No se pudo cargar la imagen A.")
-                return
-            self.show_image(self.imageA, self.labelA)
-            self.kpA, self.desA = self.extract_sift(self.imageA)
-        else:
-            QMessageBox.warning(self, "Atención", "No se seleccionó ninguna imagen A.")
-
-    def load_imageB(self):
-        file_path, _ = QFileDialog.getOpenFileName(
-            self, "Abrir Imagen B", "", "Imágenes (*.png *.jpg *.jpeg *.bmp)"
-        )
-        if file_path:
-            self.imageB = cv2.imread(file_path)
-            if self.imageB is None:
-                QMessageBox.critical(self, "Error", "No se pudo cargar la imagen B.")
-                return
-            self.show_image(self.imageB, self.labelB)
-            self.kpB, self.desB = self.extract_sift(self.imageB)
-        else:
-            QMessageBox.warning(self, "Atención", "No se seleccionó ninguna imagen B.")
 
     def show_image(self, cv_img, label):
         if cv_img is None:
